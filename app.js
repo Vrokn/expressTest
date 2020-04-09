7d7c3719f23bb63dc68b17dce9bcf023d05d3755
Mateo <mgomezossa@gmail.com>
2020-04-08 20:19:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var name = "desconocido";
 app.get('/', (req, res) => {
-  name = req.query.name;
-  res.send('<h1>"Hola "+name+"!"</h1>');
+  name = req.query.nombre;
+  res.send('<h1>Hola '+name+'!</h1>');
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
