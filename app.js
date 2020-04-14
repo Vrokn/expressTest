2e02b82581f8dab24046f76991535dfb8d3d096c
Mateo <mgomezossa@gmail.com>
2020-04-14 18:13:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-/*const express = require('express');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  let formulario =('<form action="/" method="post">'+
+    '<label for="name">'+'<input type="text" id="name" name="name">'+
+    '<button type="submit">Enviar</button>'+
+  '</form>');
+  res.send(formulario);
+});
+
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  res.send('<h1>Hola '+name+'!</h1>');
+  res.end();
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+/* COMENTARIOS DE EJERCICIOS ANTERIORES
+
+
+SALUDAME!!!
+const express = require('express');
 const app = express();
 app.get('/makers/:nombre', (req, res) => {
   let name = req.params.nombre;
   name = name[0].toUpperCase() + name.slice(1);
   (name) ? (res.send('<h1>Hola '+name+'!</h1>')) :(res.send('<h1>Hola desconocido!</h1>'));
 });
-app.listen(3000, () => console.log('Listening on port 3000!'));*/
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+par impar:
+
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
@@ -16,4 +44,4 @@ app.get('/', (req, res) => {
   }
   res.send(str);
 });
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));*/
