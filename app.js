1ae0c206915e0d1d695f91454603e7cc2807f194
Mateo <mgomezossa@gmail.com>
2020-04-13 20:38:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 app.get('/makers/:nombre', (req, res) => {
   let name = req.params.nombre;
   name = name[0].toUpperCase() + name.slice(1);
   (name) ? (res.send('<h1>Hola '+name+'!</h1>')) :(res.send('<h1>Hola desconocido!</h1>'));
+});
+app.listen(3000, () => console.log('Listening on port 3000!'));*/
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  var str = '';
+  for(let i=1;i<=50;i++){
+    i % 2 === 0 ? x =' Soy Par!' : x = ' Soy Impar!';
+    str = str + '<p>'+i+x+'</>';
+  }
+  res.send(str);
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
